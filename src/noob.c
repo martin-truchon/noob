1522c92a1536c9685b167995bfbffd2a87565746
Martin Truchon <martin.truchon95@gmail.com>
2024-08-18 20:31:49 -0400

    Move displayed content to an append buffer
    - Content that should be rendered on the screen should now be appended
      to the append buffer
    - Display the start of each rows with a tilde
    - Display a welcome message to the user

--- src/noob.c
+++ src/noob.c
@@ -1,19 +1,37 @@
+/*** includes ***/
+
+#include <asm-generic/ioctls.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+
+#define NOOB_VERSION "1.0.0"
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define ESCAPE_SEQ(s) "\x1b[" s
 
-void editor_clear_screen();
+/*** data ***/
 
-struct termios orig_termios;
+struct editorConfig
+{
+	int screen_rows;
+	int screen_cols;
+	struct termios orig_termios;
+};
+
+struct editorConfig editor;
+
+/*** terminal ***/
 
 void die(const char *s)
 {
-	editor_clear_screen();
+	write(STDOUT_FILENO, ESCAPE_SEQ("2J"), 4);
+	write(STDOUT_FILENO, ESCAPE_SEQ("H"), 3);
 
 	perror(s);
 	exit(1);
@@ -21,7 +39,7 @@ void die(const char *s)
 
 void disable_raw_mode()
 {
-	int set_attr_result = tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+	int set_attr_result = tcsetattr(STDIN_FILENO, TCSAFLUSH, &editor.orig_termios);
 	if (set_attr_result == -1) {
 		die("Error while trying to call tcsetattr");
 	}
@@ -29,13 +47,13 @@ void disable_raw_mode()
 
 void enable_raw_mode()
 {
-	int get_attr_result = tcgetattr(STDIN_FILENO, &orig_termios);
+	int get_attr_result = tcgetattr(STDIN_FILENO, &editor.orig_termios);
 	if (get_attr_result == -1) {
 		die("Error while trying to call tcgetattr");
 	}
 
 	atexit(disable_raw_mode);
-	struct termios raw = orig_termios;
+	struct termios raw = editor.orig_termios;
 	raw.c_iflag &= ~(IXON); // Turn off software flow control
 	raw.c_oflag &= ~(OPOST); // Turn off output processing
 	raw.c_lflag &= ~(ECHO | ICANON); // Turn off echoing and canonical mode
@@ -59,42 +77,131 @@ char editor_read_key()
 	return c;
 }
 
-void editor_draw_rows()
+int get_window_size(int *rows, int *cols)
 {
-	for (int i = 0; i < 24; i++) {
-		write(STDOUT_FILENO, "~\r\n", 3);
+	struct winsize size;
+
+	int ioctl_result = ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
+	if (ioctl_result == -1 || size.ws_col == 0) {
+		return -1;
+	}
+	else {
+		*rows = size.ws_row;
+		*cols = size.ws_col;
+		return 0;
 	}
 }
 
-void editor_clear_screen()
+/** append buffer ***/
+
+struct append_buffer
 {
-	write(STDOUT_FILENO, ESCAPE_SEQ("2J"), 4);
-	write(STDOUT_FILENO, ESCAPE_SEQ("H"), 3);
+	char *buffer;
+	int len;
+};
+
+void ab_append(struct append_buffer *ab, const char *s, int len)
+{
+	char *new = realloc(ab->buffer, ab->len + len);
+
+	if (new == NULL) return;
+	memcpy(&new[ab->len], s, len);
+	ab->buffer = new;
+	ab->len += len;
+}
+
+void ab_free(struct append_buffer *ab)
+{
+	free(ab->buffer);
+}
+
+/*** output ***/
+
+void editor_draw_welcome(struct append_buffer *ab)
+{
+	char message[80];
+	int message_len = snprintf(
+		message,
+		sizeof(message),
+		"NOOB editor -- version %s",
+		NOOB_VERSION
+	);
+
+	if (message_len > editor.screen_cols) {
+		message_len = editor.screen_cols;
+	}
+
+	int padding = (editor.screen_cols - message_len) / 2;
+	if (padding) {
+		ab_append(ab, "~", 1);
+		padding--;
+	}
+	while (padding--) {
+		ab_append(ab, " ", 1);
+	}
+	ab_append(ab, message, message_len);
+}
+
+void editor_draw_rows(struct append_buffer *ab)
+{
+	for (int i = 0; i < editor.screen_rows; i++) {
+		if (i == editor.screen_rows / 3)
+			editor_draw_welcome(ab);
+		else
+			ab_append(ab, "~", 1);
+
+		ab_append(ab, ESCAPE_SEQ("K"), 3);
+		if (i < editor.screen_rows - 1) {
+			ab_append(ab, "\r\n", 2);
+		}
+	}
 }
 
 void editor_refresh_screen()
 {
-	editor_clear_screen();
-	editor_draw_rows();
+	struct append_buffer ab = { NULL, 0 };
 
-	write(STDOUT_FILENO, ESCAPE_SEQ("H"), 3);
+	ab_append(&ab, ESCAPE_SEQ("?25l"), 6);
+	ab_append(&ab, ESCAPE_SEQ("H"), 3);
+
+	editor_draw_rows(&ab);
+
+	ab_append(&ab, ESCAPE_SEQ("H"), 3);
+	ab_append(&ab, ESCAPE_SEQ("?25h"), 6);
+
+	write(STDOUT_FILENO, ab.buffer, ab.len);
+	ab_free(&ab);
 }
+
+/*** input ***/
 
 void editor_process_keypress()
 {
 	char c = editor_read_key();
 
 	switch (c) {
-		case CTRL_KEY('q'): 
-			editor_clear_screen();
+		case CTRL_KEY('q'):
+			write(STDOUT_FILENO, ESCAPE_SEQ("2J"), 4);
+			write(STDOUT_FILENO, ESCAPE_SEQ("H"), 3);
 			exit(0);
 			break;
+	}
+}
+
+/*** init ***/
+
+void init_editor()
+{
+	int result = get_window_size(&editor.screen_rows, &editor.screen_cols);
+	if (result == -1) {
+		die("Error while trying to get the window size");
 	}
 }
 
 int main()
 {
 	enable_raw_mode();
+	init_editor();
 	while (1) {
 		editor_refresh_screen();
 		editor_process_keypress();
